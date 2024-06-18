1755dbdc9f11ff0956d4dbdc635f6e1884815652
gregorio-calabretta <gregoriocalabretta94@gmail.com>
2024-06-18 12:19:47 +0200

    first commit

---
+++ src/test/java/com/onlineStore/TechBits/TechBitsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.onlineStore.TechBits;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TechBitsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
